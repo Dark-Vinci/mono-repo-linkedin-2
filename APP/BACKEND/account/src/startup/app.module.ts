f958da5a13aa066bdb89b19328ecd77d7381b871
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-09-11 16:48:06 +0100

    chore: more and mpre

--- APP/BACKEND/account/src/startup/app.module.ts
+++ APP/BACKEND/account/src/startup/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ShutdownService } from './app.shutdown.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ShutdownService],
 })
 export class AppModule {}
