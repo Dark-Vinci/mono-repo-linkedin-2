1d19b4ce8cf298d6d28ebed2477be3ea006d7e13
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-09-11 23:03:30 +0100

    hore:more work

--- APP/BACKEND/SDK/src/grpc/generated/auth/request_pb.d.ts
+++
@@ -1,5 +0,0 @@
-// package: 
-// file: auth/request.proto
-
-import * as jspb from "google-protobuf";
-
--- APP/BACKEND/SDK/src/grpc/generated/auth/response_pb.d.ts
+++
@@ -1,5 +0,0 @@
-// package: 
-// file: auth/response.proto
-
-import * as jspb from "google-protobuf";
-
