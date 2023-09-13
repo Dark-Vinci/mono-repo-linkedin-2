5b54b11f297b47b3c1e17eb60ffbd5d0f6f5af85
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-09-13 13:25:41 +0100

    chore: we doing more

---
+++ APP/FRONTEND/application/src/router/index.tsx
@@ -0,0 +1,98 @@
+import { createBrowserRouter, useLoaderData } from 'react-router-dom';
+
+import style from './styles/App.module.css';
+
+// let [searchParams, setSearchParams] = useSearchParams();
+//   let paramValue = searchParams.get(key);
+
+// let resolved = useResolvedPath(to);
+//   let match = useMatch({ path: resolved.pathname, end: true });
+
+// let [searchParams] = useSearchParams();
+//   let isActive = searchParams.get("brand") === brand;
+// let { id } = useParams<"id">();
+
+function router(props: { isVerified: boolean }) {
+  return createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <div> saving a brother </div>,
+        loader: () => 'na so',
+        children: [
+          {
+            index: true,
+            loader: () => ({ message: 'what is going wrong' }),
+            Component: () => {
+              const data = useLoaderData() as { message: string };
+              return <div className={style.App}>{data.message}</div>;
+            },
+
+            // Component: <div>This is the componenet to be redered</div>,
+            // action: todosAction,
+            // loader: todosLoader,
+            // Component: TodosList,
+            // ErrorBoundary: TodosBoundary,
+            // children: [
+            //   {
+            //     path: ":id",
+            //     loader: todoLoader,
+            //     Component: Todo,
+            //   },
+            // ],
+
+            // async lazy() {
+            //   let { dashboardMessagesLoader, DashboardMessages } = await import(
+            //     "./pages/Dashboard"
+            //   );
+            //   return {
+            //     loader: dashboardMessagesLoader,
+            //     Component: DashboardMessages,
+            //   };
+            // },
+          },
+
+          {
+            path: '/about',
+            errorElement: <div>Error Element</div>,
+
+            async lazy() {
+              const { ErrorBoundary } = await import('@components');
+              console.log({ ErrorBoundary });
+
+              return {
+                Component: <div>This is the about page</div>,
+                loader: {},
+              } as any;
+            },
+
+            children: [
+              props.isVerified
+                ? {
+                    path: '/:id',
+                    element: <div>This is the about page</div>,
+                    loader: () => ({ message: 'what is going wrong' }),
+                    index: true,
+
+                    Component: () => {
+                      const data = useLoaderData() as { message: string };
+                      return <div className={style.App}>{data.message}</div>;
+                    },
+                  }
+                : {},
+            ],
+          },
+
+          {
+            path: '*',
+            element: <div>Page 404</div>,
+          },
+        ],
+      },
+    ],
+
+    { basename: '/my-app' },
+  );
+}
+
+export default router;
