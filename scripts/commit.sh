c556c40edfa9e3d64074c9ef29dc89c16b0d900e
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-06 23:32:32 +0100

    /Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/mono-repo-linkedin/scripts/commit.sh:chore:

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "commit command will run now"
+echo "commit command will run now";
 
-echo "$1"
-echo "$2"
+echo "commit type: [$1]";
+echo "commit message: [$2]";
 
 git add .;
-git commit -am '$0:$1';
+git commit -am "$0:$1";
 
 echo "files now committed"
 
