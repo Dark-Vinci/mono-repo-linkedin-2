a75400aed61f85ad428c930d56621f0848b70c11
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-06 23:40:54 +0100

    /Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/mono-repo-linkedin/scripts/commit.sh:

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,11 +5,13 @@ echo "commit command will run now";
 echo "commit type: [$1]";
 echo "commit message: [$2]";
 
+# add to [] and commit
 git add .;
 git commit -am "$0:$1";
 
 echo "files now committed"
 
+# push to github
 git push;
 
-echo 'file pushed to github';
+echo "file pushed to github";
--- scripts/run-format.sh
+++ scripts/run-format.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+echo "we need to run format for every services";
+
+#for i in ./APP/BACKEND/**; do
+#  echo "$i"
+#done
+
+for dir in ./APP/BACKEND/; do
+    echo "$dir"
+done
