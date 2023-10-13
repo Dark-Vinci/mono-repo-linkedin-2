e103f60286757d06004be3dc1617b6df02d86d9e
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-13 09:47:40 +0100

    chore:we have more things to cleanup

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,6 +5,19 @@ echo "commit command will run now";
 echo "commit type: [$1]";
 echo "commit message: [$2]";
 
+directory = '../APP/BACKEND';
+
+
+for diren in "$directory"; do
+    echo $diren
+#    if [ -d "$diren/node_modules" ]; then
+#     echo "node modules exists ====> Removing now"
+#     rm -rf $diren/node_modules
+#    else
+#     echo "The directory does not exist ====> well pass"
+#    fi
+done
+
 # add to [] and commit
 git add .;
 sleep 2
