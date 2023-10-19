41b4564ba7ee37da36663628c6c880c81de218b5
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-19 13:55:01 +0100

    chore: w100%

--- APP/FRONTEND/application/src/components/HOC/aux.tsx
+++ APP/FRONTEND/application/src/components/HOC/aux.tsx
@@ -1,6 +1,6 @@
 import { AuxProps } from "@types";
 
-import React, {ReactNode} from "react";
+import {ReactNode} from "react";
 
 function Aux<P extends AuxProps<ReactNode>>(props: P): ReactNode {
     return props.children
