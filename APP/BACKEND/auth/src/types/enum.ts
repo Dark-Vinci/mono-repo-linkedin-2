a6c7e015013b953d1336a5045811fb2b9dfdf42f
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-19 13:52:05 +0100

    chore: more

---
+++ APP/BACKEND/auth/src/types/enum.ts
@@ -0,0 +1,32 @@
+export enum RedisTimerType {
+  EX = 'EX',
+}
+
+export enum AuthDatabase {
+  MASTER = 'auth_master',
+  SLAVE1 = 'auth_slave1',
+  SLAVE2 = 'auth_slave2',
+  SLAVE3 = 'auth_slave3',
+}
+
+export enum DBPassword {
+  MASTER = 'dbMasterPassword',
+  SLAVE = 'dbSlavePassword',
+}
+
+export enum ColumnType {
+  VARCHAR = 'varchar',
+  TIMESTAMP = 'timestamp',
+  UUID = 'uuid',
+  INT = 'int',
+}
+
+export enum Ordering {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
+
+export enum EntityNames {
+  USERS = 'users',
+  BUSINESSES = 'businesses',
+}
---
+++ APP/BACKEND/auth/src/types/type.ts
@@ -0,0 +1,24 @@
+import { FactoryProvider, ModuleMetadata } from '@nestjs/common';
+import Redis, { RedisOptions } from 'ioredis';
+
+export type OrNull<Type> = Type | null;
+
+export type OneOrMany<Type> = Type | Type[];
+
+export type OneOrManyOrNull<Type> = OrNull<OneOrMany<Type>>;
+
+export type NodeEnvType = 'development' | 'production' | 'test';
+
+export type DBType = 'postgres';
+
+export type RedisModuleOptions = {
+  connectionOptions: RedisOptions;
+  onClientReady?: (client: Redis) => void;
+};
+
+export type RedisAsyncModuleOptions = {
+  useFactory: (
+    ...args: any[]
+  ) => Promise<RedisModuleOptions> | RedisModuleOptions;
+} & Pick<ModuleMetadata, 'imports'> &
+  Pick<FactoryProvider, 'inject'>;
