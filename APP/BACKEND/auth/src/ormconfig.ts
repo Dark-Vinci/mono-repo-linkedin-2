e464e47e0e141520a6a3c89210dc8730ec946036
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-21 21:11:43 +0100

    chore: more work

--- APP/BACKEND/auth/src/ormconfig.ts
+++ APP/BACKEND/auth/src/ormconfig.ts
@@ -1,47 +1,17 @@
 import { DataSource } from 'typeorm';
+import { Business } from './models/business';
 
-export default new DataSource({
-  // type: 'postgres',
-  // host: 'localhost',
-  // port: 5420,
-  // database: 'my_database',
-
-  // entities: ['dist/models/*.{js, ts}'],
-  // migrations: ['migrations/*{.js,.ts}'],
-  // username: 'docker',
-  // password: 'docker',
-  // synchronize: false,
-
-  // {
+const dataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5420,
   database: 'my_database',
 
-  entities: ['src/models/*.{js, ts}'],
+  entities: [Business],
   migrations: ['migrations/*{.js,.ts}'],
   username: 'docker',
   password: 'docker',
   synchronize: false,
 });
 
-// ormconfig.ts
-// exports default const = {
-//   type: 'postgres',
-//   host: 'localhost',
-//   port: 5420,
-//   database: 'my_database',
-
-//   entities: ['dist/models/*.{js, ts}'],
-//   migrations: ['migrations/*{.js,.ts}'],
-//   username: 'docker',
-//   password: 'docker',
-//   synchronize: false,
-//   cli: {
-//     migrationsDir: 'src/migration',
-//   },
-// };
-
-// console.log({ AppDataSource: AppDataSource.options });
-
-// export default AppDataSource;
+export default dataSource;
--- scripts/generate-migration.sh
+++ scripts/generate-migration.sh
@@ -2,8 +2,8 @@
 
 echo 'who are you'
 
-if [ 1 = 2 ]; then
-    echo 'one is truly equal one'
-elif [ 3 = 3 ]; then
-      echo 'we are the best music'
-fi
+# if [ 1 = 2 ]; then
+#     echo 'one is truly equal one'
+# elif [ 3 = 3 ]; then
+#       echo 'we are the best music'
+# fi
