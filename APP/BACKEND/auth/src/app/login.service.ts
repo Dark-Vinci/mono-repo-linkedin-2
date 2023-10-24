9724273c90748e9a1fd83f8b1153c8eb0b8a8c42
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-24 08:45:36 +0100

    chore: more work

--- APP/BACKEND/auth/src/app/login.service.ts
+++ APP/BACKEND/auth/src/app/login.service.ts
@@ -1,4 +1,18 @@
+import global from 'globals';
+
 import { Injectable } from '@nestjs/common';
+import winston from 'winston';
+
+import { JwtAuthService } from './jwt.service';
 
 @Injectable()
-export class SignInService {}
+export class SignInService {
+  private logger: winston.Logger | any;
+
+  public constructor(private readonly jwtAuthService: JwtAuthService) {}
+
+  public onApplicationBootstrap(): void {
+    this.logger = global.logger;
+    console.log({ a: this.logger, b: this.jwtAuthService });
+  }
+}
