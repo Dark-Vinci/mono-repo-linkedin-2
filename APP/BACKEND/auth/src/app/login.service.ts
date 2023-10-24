c3f02cf89e5e79b6d00f2dea1974828c96f2938e
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-24 10:36:31 +0100

    chore: more and more

--- APP/BACKEND/auth/src/app/login.service.ts
+++ APP/BACKEND/auth/src/app/login.service.ts
@@ -3,16 +3,21 @@ import global from 'globals';
 import { Injectable } from '@nestjs/common';
 import winston from 'winston';
 
+import { RedisClient } from '@connections';
+
 import { JwtAuthService } from './jwt.service';
 
 @Injectable()
 export class SignInService {
   private logger: winston.Logger | any;
 
-  public constructor(private readonly jwtAuthService: JwtAuthService) {}
+  public constructor(
+    private readonly jwtAuthService: JwtAuthService,
+    private readonly redis: RedisClient,
+  ) {}
 
   public onApplicationBootstrap(): void {
     this.logger = global.logger;
-    console.log({ a: this.logger, b: this.jwtAuthService });
+    console.log({ a: this.logger, b: this.jwtAuthService, c: this.redis });
   }
 }
--- APP/BACKEND/auth/src/app/signup.service.ts
+++ APP/BACKEND/auth/src/app/signup.service.ts
@@ -3,16 +3,21 @@ import global from 'globals';
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import winston from 'winston';
 
+import { RedisClient } from '@connections';
+
 import { JwtAuthService } from './jwt.service';
 
 @Injectable()
 export class SignUpService implements OnApplicationBootstrap {
   private logger: winston.Logger | any;
 
-  public constructor(private readonly jwtAuthService: JwtAuthService) {}
+  public constructor(
+    private readonly jwtAuthService: JwtAuthService,
+    private readonly redis: RedisClient,
+  ) {}
 
   public onApplicationBootstrap(): void {
     this.logger = global.logger;
-    console.log({ a: this.logger, b: this.jwtAuthService });
+    console.log({ a: this.logger, b: this.jwtAuthService, c: this.redis });
   }
 }
