7b166bab88982dead190421375ba6e4d4e51616e
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-10-28 18:48:41 +0100

    chore: more cleanups

--- APP/BACKEND/SDK/src/grpc/generate.sh
+++ APP/BACKEND/SDK/src/grpc/generate.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# npm proto-loader-gen-types --grpcLib=@grpc/grpc-js --outDir=generated/ **/*.proto
-# protoc --go_out=plugins=grpc:. --ts_opt=paths=source_relative ./banking/*.proto
-
-
-# protoc -I=sourcedir --ts_out=dist myproto.proto
-
-# protoc -I=. --ts_out=generated ./auth/*.proto
-# protoc  --ts_opt=target=node -I=. --ts_out=generated ./auth/*.proto
-
 protoc --plugin=/Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/mono-repo-linkedin/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. ./auth/*.proto
 protoc --plugin=/Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/mono-repo-linkedin/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. ./account/*.proto
 protoc --plugin=/Users/ademolaolutomiwaabraham/Desktop/allcodes/work-go/src/mono-repo-linkedin/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. ./models/*.proto
