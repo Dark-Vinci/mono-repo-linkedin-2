ce46977de55bc869a537270fb2fa6a9e8c4409f2
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-11-01 11:33:35 +0100

    chore: ore;

--- APP/BACKEND/new-auth/src/store/index.ts
+++ APP/BACKEND/new-auth/src/store/index.ts
@@ -1,3 +1,13 @@
 export * from './user.service';
 export * from './business.service';
+export * from './activities.store';
+export * from './experiences.store';
+export * from './license.store';
+export * from './project.store';
+export * from './skills-endorsement.store';
+export * from './skills.store';
+export * from './user-skill.store';
+export * from './user.service';
+export * from './volunteering.store';
+
 export { StoreModule as default } from './store.module';
