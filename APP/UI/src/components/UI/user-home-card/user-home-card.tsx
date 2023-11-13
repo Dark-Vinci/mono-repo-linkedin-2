681ee923bb300abcb30d78ec29755904c43ef517
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-11-13 22:42:28 +0100

    chore: more on user cards

--- APP/UI/src/components/UI/user-home-card/user-home-card.tsx
+++ APP/UI/src/components/UI/user-home-card/user-home-card.tsx
@@ -1,53 +1,74 @@
-import React from 'react';
+import React from "react";
 
-import styles from './user-home-card.module.scss';
+import styles from "./user-home-card.module.scss";
+import type { UserHomeCardProps } from "@types";
 
-interface UserHomeCardProps {
-    readonly profileUrl: string;
-    readonly fullName: string;
-    readonly profileViewers: number;
-    readonly connections: number;
-    readonly title: string;
-    readonly backgroundImage: string;
-}
+export function UserHomeCard({
+  profileUrl,
+  backgroundImage,
+  fullName,
+  title,
+  profileViewers,
+  connections,
+}: UserHomeCardProps): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <div className="top">
+        <div className="top_container">
+          <div
+            className="header"
+            style={{
+              backgroundImage,
+            }}
+          ></div>
 
-export function UserHomeCard({profileUrl, backgroundImage, fullName, title }: UserHomeCardProps): JSX.Element {
-    return (
-        <div className={styles.container}>
-            <div className="top">
-                <div className="top_container">
-                    <div className="header"></div>
-                    <div className="img">
-                        <img src={ profileUrl } alt="profile-url" />
-                    </div>
-                    <div className="name">{ fullName }</div>
-                    <div className="title">{ title }</div>
-                </div>
-            </div>
-
-            <div className="middle">
-                <div className="middle_container">
-                    <div className="profile_viewer"></div>
-                    <div className="container"></div>
-                </div>
-            </div>
-
-            <div className="lower">
-                <div className="lower_container">
-                    <p></p>
-                    <div className="premium">
-                        <div className="icon"></div>
-                        <p><a href=".">Try Premium For Free</a></p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="footer">
-                <div className="footer_container">
-                    <div className="icon"></div>
-                    <p>My Items</p>
-                </div>
-            </div>
+          <div className="img">
+            <img src={profileUrl} alt="profile-url" />
+          </div>
+          <div className="name">{fullName}</div>
+          <div className="title">{title}</div>
         </div>
-    );
+      </div>
+
+      <div className="middle">
+        <div className="middle_container">
+          <div className="profile_viewer">
+            <div className="profile_viewers">
+              <p>Profile viewers</p>
+              <p>{profileViewers}</p>
+            </div>
+
+            <div className="connections">
+              <div>
+                <p>Connections</p>
+                <p>{connections}</p>
+              </div>
+
+              <div>manage your network</div>
+            </div>
+          </div>
+          <div className="container"></div>
+        </div>
+      </div>
+
+      <div className="lower">
+        <div className="lower_container">
+          <p></p>
+          <div className="premium">
+            <div className="icon"></div>
+            <p>
+              <a href=".">Try Premium For Free</a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="footer_container">
+          <div className="icon"></div>
+          <p>My Items</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- APP/UI/src/types/interface.ts
+++ APP/UI/src/types/interface.ts
@@ -6,3 +6,12 @@ export interface HomeProps {
 export interface UG {
     readonly profileUrl: string;
 }
+
+export interface UserHomeCardProps {
+    readonly profileUrl: string;
+    readonly fullName: string;
+    readonly profileViewers: number;
+    readonly connections: number;
+    readonly title: string;
+    readonly backgroundImage: string;
+}
