64570d5e3ee248ff1b93983a8e6fa1f0014ab71a
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-11-14 08:39:30 +0100

    chore: more on page navigatyions

---
+++ APP/UI/src/pages/404/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './404.module.scss';
+
+export function Page404(): JSX.Element {
+    return (
+        <div className={ style.container }>
+            page404
+        </div>
+    );
+}
