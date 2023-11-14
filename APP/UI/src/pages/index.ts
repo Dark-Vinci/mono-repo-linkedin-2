4c8ece8feb6e8435d53ddb16dd43d2f31dfcfe1e
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-11-14 08:39:24 +0100

    chore: more on page navigatyions

--- APP/UI/src/pages/index.ts
+++ APP/UI/src/pages/index.ts
@@ -1,1 +1,6 @@
 export * from './home';
+export * from './notification';
+export * from './jobs';
+export * from './me';
+export * from './messaging';
+export * from './404';
--- APP/UI/src/router/router.tsx
+++ APP/UI/src/router/router.tsx
@@ -1,71 +1,31 @@
-import { createBrowserRouter, NavLink } from 'react-router-dom';
-import React, { JSX } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import React from 'react';
+
+import { Home, Jobs, Me, Messaging, Notification, Page404 } from '@pages';
 
 export const router = createBrowserRouter([
   {
-    path: 'home',
-    element: <HomePage />,
+    path: '/me',
+    element: <Me />,
   },
   {
-    path: 'about',
-    element: <AboutPage />,
+    path: '/messaging',
+    element: <Messaging />,
   },
   {
-    path: 'contact',
-    element: <ContactPage />,
+    path: '/jobs',
+    element: <Jobs />,
   },
   {
-    path: '',
-    element: <MainApp />
+    path: '/home',
+    element: <Home />
+  },
+  {
+    path: '/notification',
+    element: <Notification />
   },
   {
     path: '*',
-    element: <DefaultElement/>,
+    element: <Page404/>,
   }
 ]);
-
-function MainApp() {
-  return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li><NavLink to="/home">Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
-        </ul>
-      </nav>
-      <main>This is the main page</main>
-    </div>
-  );
-}
-
-function HomePage() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to my home page.</p>
-    </div>
-  );
-}
-
-function AboutPage() {
-  return (
-    <div>
-      <h1>About Page</h1>
-      <p>Learn more about me.</p>
-    </div>
-  );
-}
-
-function ContactPage() {
-  return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>Get in touch with me.</p>
-    </div>
-  );
-}
-
-function DefaultElement(): JSX.Element {
-  return <div>Page 404</div>;
-}
