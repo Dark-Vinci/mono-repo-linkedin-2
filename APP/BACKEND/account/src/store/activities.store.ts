53ccb0d16bf1ce009446493b15b2156a8f154f30
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-11-17 21:34:29 +0100

    chore; more to be done

--- APP/BACKEND/account/src/store/activities.store.ts
+++ APP/BACKEND/account/src/store/activities.store.ts
@@ -2,26 +2,131 @@ import global from 'globals';
 
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Logger as WinstonLogger } from 'winston';
-import { Repository } from 'typeorm';
+import {
+  FindManyOptions,
+  FindOneOptions,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
 
-import { LoggerDecorator, Undefinable } from 'sdk';
+import {
+  AuthDatabase,
+  LoggerDecorator,
+  Undefinable,
+  Util,
+  entityId,
+  genericGet,
+  partialEntity,
+  updateEntity,
+} from 'sdk';
 
-import { Activities, User } from '@models';
+import { Activities } from '@models';
 
 @Injectable()
 export class ActivitiesStore {
-  public globalLogger: Undefinable<WinstonLogger>;
+  private slaveRepositories: Undefinable<Repository<Activities>[]>;
 
   public constructor(
-    @InjectRepository(Activities)
+    @InjectRepository(Activities, AuthDatabase.MASTER)
     private readonly masterRepository: Repository<Activities>,
-  ) {}
+
+    @InjectRepository(Activities, AuthDatabase.SLAVE1)
+    slave1Repository: Repository<Activities>,
+
+    @InjectRepository(Activities, AuthDatabase.SLAVE2)
+    slave2Repository: Repository<Activities>,
+
+    @InjectRepository(Activities, AuthDatabase.SLAVE3)
+    slave3Repository: Repository<Activities>,
+
+    private readonly util: Util,
+  ) {
+    this.slaveRepositories = [
+      slave3Repository,
+      slave2Repository,
+      slave1Repository,
+    ];
+  }
 
   @LoggerDecorator(global.logger, __filename)
-  public async createUser(): Promise<Activities> {
-    this.globalLogger = global.logger;
+  public async create({
+    payload,
+  }: partialEntity<Activities>): Promise<Activities> {
+    const activity = this.masterRepository.create(payload);
 
-    return new Activities();
+    await activity.save();
+
+    return activity;
+  }
+
+  public async softDelete({ id }: entityId<Activities>): Promise<boolean> {
+    await this.masterRepository.softDelete(id);
+
+    return true;
+  }
+
+  @LoggerDecorator(global.Logger, __filename)
+  public async genericGet({
+    paginateOptions,
+    payload,
+    requestId,
+  }: genericGet<Activities>): Promise<Array<Activities>> {
+    const strPayload = JSON.stringify(payload);
+
+    const findObj: FindManyOptions<Activities> = {
+      where: { ...(payload as unknown as FindOptionsWhere<Activities>[]) },
+
+      take: paginateOptions.size,
+      skip: paginateOptions.skip,
+
+      order: { createdAt: 'ASC' },
+      comment: `get activity that matches ${strPayload} by pagination strategy with requestId ${requestId}`,
+    };
+
+    const findMap = this.slaveRepositories!.map(
+      (repo: Repository<Activities>) => {
+        return repo.find(findObj);
+      },
+    );
+
+    const activities = await Promise.any(findMap);
+
+    return activities;
+  }
+
+  @LoggerDecorator(global.Logger, __filename)
+  public async update({
+    update,
+    toBeUpdated,
+  }: updateEntity<Activities>): Promise<boolean> {
+    // move all the update to the activity object <-
+    Object.assign(toBeUpdated, update);
+
+    this.util.assert(!!toBeUpdated.id, 'activity id is not provided');
+
+    await this.masterRepository.update(toBeUpdated.id!, toBeUpdated);
+
+    return true;
+  }
+
+  @LoggerDecorator(global.Logger, __filename)
+  public async getOne(payload: Partial<Activities>): Promise<Activities> {
+    const strPayload = JSON.stringify(payload);
+
+    const findOneOrFailOptions: FindOneOptions = {
+      where: { ...payload },
+
+      comment: `find one activity with details ${strPayload} or fail`,
+    };
+
+    const findMap = this.slaveRepositories!.map(
+      (repo: Repository<Activities>) => {
+        return repo.findOneOrFail(findOneOrFailOptions);
+      },
+    );
+
+    const activity = await Promise.any(findMap);
+
+    return activity;
   }
 }
--- APP/BACKEND/account/src/store/business.store.ts
+++ APP/BACKEND/account/src/store/business.store.ts
@@ -1,39 +1,130 @@
 import global from 'globals';
 
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Logger as WinstonLogger } from 'winston';
+import {
+  FindManyOptions,
+  FindOneOptions,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
 
-import { Util, UUID, Undefinable } from 'sdk';
+import {
+  Util,
+  Undefinable,
+  AuthDatabase,
+  LoggerDecorator,
+  entityId,
+  genericGet,
+  partialEntity,
+  updateEntity,
+} from 'sdk';
 
 import { Business } from '@models';
 
 @Injectable()
-export class BusinessRepository implements OnApplicationBootstrap {
-  public globalLogger: Undefinable<WinstonLogger>;
+export class BusinessStore {
+  private slaveRepositories: Undefinable<Repository<Business>[]>;
 
   public constructor(
-    @InjectRepository(Business)
-    private readonly _businessRepository: Repository<Business>,
-    private readonly util: Util,
-  ) {}
+    @InjectRepository(Business, AuthDatabase.MASTER)
+    private readonly masterRepository: Repository<Business>,
 
-  public onApplicationBootstrap(): void {
-    this.globalLogger = global.logger;
+    @InjectRepository(Business, AuthDatabase.SLAVE1)
+    slave1Repository: Repository<Business>,
+
+    @InjectRepository(Business, AuthDatabase.SLAVE2)
+    slave2Repository: Repository<Business>,
+
+    @InjectRepository(Business, AuthDatabase.SLAVE3)
+    slave3Repository: Repository<Business>,
+
+    private readonly util: Util,
+  ) {
+    this.slaveRepositories = [
+      slave3Repository,
+      slave2Repository,
+      slave1Repository,
+    ];
   }
 
-  public async _createBusiness(
-    _payload: Partial<Business>,
-    _requestId: UUID,
-  ): Promise<void> {
-    console.log({
-      a: this._businessRepository,
-      b: this.globalLogger,
-      c: this.util,
-      d: _payload,
-      e: _requestId,
-    });
-    return;
+  @LoggerDecorator(global.logger, __filename)
+  public async create({ payload }: partialEntity<Business>): Promise<Business> {
+    const business = this.masterRepository.create(payload);
+
+    await business.save();
+
+    return business;
+  }
+
+  public async softDelete({ id }: entityId<Business>): Promise<boolean> {
+    await this.masterRepository.softDelete(id);
+
+    return true;
+  }
+
+  @LoggerDecorator(global.Logger, __filename)
+  public async genericGet({
+    paginateOptions,
+    payload,
+    requestId,
+  }: genericGet<Business>): Promise<Array<Business>> {
+    const strPayload = JSON.stringify(payload);
+
+    const findObj: FindManyOptions<Business> = {
+      where: { ...(payload as unknown as FindOptionsWhere<Business>[]) },
+
+      take: paginateOptions.size,
+      skip: paginateOptions.skip,
+
+      order: { createdAt: 'ASC' },
+      comment: `get business that matches ${strPayload} by pagination strategy with requestId ${requestId}`,
+    };
+
+    const findMap = this.slaveRepositories!.map(
+      (repo: Repository<Business>) => {
+        return repo.find(findObj);
+      },
+    );
+
+    const businesses = await Promise.any(findMap);
+
+    return businesses;
+  }
+
+  @LoggerDecorator(global.Logger, __filename)
+  public async update({
+    update,
+    toBeUpdated,
+  }: updateEntity<Business>): Promise<boolean> {
+    // move all the update to the business object <-
+    Object.assign(toBeUpdated, update);
+
+    this.util.assert(!!toBeUpdated.id, 'business id is not provided');
+
+    await this.masterRepository.update(toBeUpdated.id!, toBeUpdated);
+
+    return true;
+  }
+
+  @LoggerDecorator(global.Logger, __filename)
+  public async getOne(payload: Partial<Business>): Promise<Business> {
+    const strPayload = JSON.stringify(payload);
+
+    const findOneOrFailOptions: FindOneOptions = {
+      where: { ...payload },
+
+      comment: `find one business with details ${strPayload} or fail`,
+    };
+
+    const findMap = this.slaveRepositories!.map(
+      (repo: Repository<Business>) => {
+        return repo.findOneOrFail(findOneOrFailOptions);
+      },
+    );
+
+    const business = await Promise.any(findMap);
+
+    return business;
   }
 }
