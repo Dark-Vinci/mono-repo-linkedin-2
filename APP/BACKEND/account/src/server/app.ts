644478c7c504a0d13177c4d9591853b951433517
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-11-17 22:00:27 +0100

    chore: more cleanups

---
+++ APP/BACKEND/account/src/server/app.ts
@@ -0,0 +1,66 @@
+import global from 'globals';
+
+import { Controller, OnApplicationBootstrap } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Logger as WinstonLogger } from 'winston';
+import {
+  AuthPingRequest,
+  AuthPingResponse,
+  AuthService,
+  MyLogger as Logger,
+  Type,
+  Undefinable,
+  Util,
+} from 'sdk';
+
+import {
+  SERVICE_NAME,
+  fileNames,
+  appControllerMethods,
+  MethodName,
+} from '@constants';
+import { AppService } from '@startup';
+
+@Controller()
+export class AppController
+  implements
+    Pick<AuthService, MethodName.PING>,
+    AppController,
+    OnApplicationBootstrap
+{
+  public globalLogger: Undefinable<WinstonLogger>;
+
+  constructor(
+    private readonly appService: AppService,
+    private readonly util: Util,
+  ) {}
+
+  public onApplicationBootstrap(): void {
+    this.globalLogger = global.logger;
+    console.log({ util: this.util });
+  }
+
+  @GrpcMethod(SERVICE_NAME, MethodName.PING)
+  public async ping(payload: AuthPingRequest): Promise<AuthPingResponse> {
+    const { appService, globalLogger } = this;
+
+    const logger = Logger.setContext(
+      fileNames.APP_SERVICE,
+      appControllerMethods.PING,
+      payload.requestId,
+      globalLogger!,
+      payload,
+    );
+
+    logger.log('got a new ping request');
+
+    const requestUUID = appService.ping(payload);
+
+    logger.logPayload(
+      { requestUUID: requestUUID.toJSON() },
+      Type.RESPONSE_RESPONSE,
+    );
+
+    return { requestId: requestUUID.toString() };
+  }
+}
